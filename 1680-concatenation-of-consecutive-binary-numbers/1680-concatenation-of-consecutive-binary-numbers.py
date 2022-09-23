a73a537eb170e60a69b4ef2440fcc117f0f24983
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2022-09-23 13:22:07 +0900

    Time: 2586 ms (53.54%), Space: 13.8 MB (80.31%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -6,9 +6,8 @@ class Solution:
         prev = 1
         
         for i in range(2, n+1):
-            prev = prev << (math.floor(math.log2(i)+1))
-            prev = prev + i
-            prev = prev%(10**9 + 7)
+            prev <<= math.floor(math.log2(i))+1
+            prev += i
+            prev %= 10**9 + 7
             
         return prev%(10**9 + 7)
-        
