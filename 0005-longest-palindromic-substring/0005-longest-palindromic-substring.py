38091b64ba169963c4f0a07da0e21c5e42758a00
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-03 20:22:46 +0900

    Time: 332 ms (95.27%), Space: 16.4 MB (47.68%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,27 +4,16 @@ class Solution:
         n = len(s)
         
         def expand(left, right):
-            
-            while left >= 0 and right < n:
-                if s[left] == s[right]:
-                    left -= 1
-                    right += 1
-                else:
-                    break
-            
-            return [left+1, right-1]
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+                
+            return s[left+1:right-1+1]
         
         for i in range(n):
-            left, right = expand(i, i)
-            diff = right - left + 1
-            
-            if diff > len(result):
-                result = s[left:right+1]
-            
-            left, right = expand(i, i+1)
-            diff = right - left + 1
-            
-            if diff > len(result):
-                result = s[left:right+1]
+            odd_window = expand(i, i)
+            even_window = expand(i, i+1)
+        
+            result = max(result, odd_window, even_window, key=len)
             
         return result
