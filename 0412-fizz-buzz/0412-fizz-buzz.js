98cb64f9b18b337254e60c4b7fe2c46a18511be9
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-06 17:25:53 +0900

    Time: 61 ms (79.12%), Space: 44.2 MB (71.5%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,15 +3,12 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    const result = Array(n).fill({ value: "" });
+    return Array.from({ length: n }, (_, i) => {
+        i += 1;
 
-    result.forEach((el, i) => {
-        let j = i+1;
-        if (j % 3 === 0 && j % 5 === 0) result[i] = "FizzBuzz";
-        else if (j % 3 === 0) result[i] = "Fizz";
-        else if (j % 5 === 0) result[i] = "Buzz";
-        else result[i] = String(j);
-    })
-
-    return result;
+        return (i % 3 === 0 && i % 5 === 0) ? "FizzBuzz"
+            : (i % 3 === 0) ? "Fizz"
+            : (i % 5 === 0) ? "Buzz"
+            : "" + i;
+    });
 };
