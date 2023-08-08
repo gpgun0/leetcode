b24aa9bf829d64ed7a6fe43629c826221aac56a3
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-09 02:01:53 +0900

    Time: 44 ms (94.08%), Space: 16.8 MB (6.51%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,31 +1,31 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = len(nums)
+
         left = 0
         right = l-1
 
         while left <= right:
             mid = (left + right) // 2
 
-            if nums[mid] > nums[-1]:
-                left = mid + 1
-            else:
+            if nums[mid] <= nums[-1]:
                 right = mid - 1
+            else:
+                left = mid + 1
         
-        # left - 1 -> pivot index
+        # left -> 가장 작은 값
 
         def binary_search(left, right, target):
-            mid = (left + right) // 2
 
             while left <= right:
                 mid = (left + right) // 2
-                
+
                 if nums[mid] == target:
                     return mid
                 elif nums[mid] < target:
-                    left = mid + 1
+                    left += 1
                 else:
-                    right = mid - 1
+                    right -= 1
             
             return -1
         
@@ -33,5 +33,8 @@ class Solution:
 
         if result != -1:
             return result
-        
-        return binary_search(left, l-1, target)
+
+        return binary_search(left, l - 1, target)
+
+
+
