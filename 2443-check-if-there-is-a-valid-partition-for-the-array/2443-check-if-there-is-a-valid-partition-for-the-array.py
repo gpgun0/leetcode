85df65de4be26f1064869cd4d51c3e38ca7aaabc
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-13 15:52:35 +0900

    Time: 981 ms (74.62%), Space: 31.5 MB (59.23%) - LeetHub

--- 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,8 +1,20 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
+        nums = [0] + nums
         n = len(nums)
-        memo = {}
+        dp = [0]*n
+        dp[0] = 1
 
+        for i in range(1, n):
+            if i >= 2 and nums[i] == nums[i-1]:
+                dp[i] |= dp[i-2]
+            if i >= 3 and nums[i] == nums[i-1] == nums[i-2]:
+                dp[i] |= dp[i-3]
+            if i >= 3 and nums[i] == nums[i-1]+1 == nums[i-2]+2:
+                dp[i] |= dp[i-3]
+
+        return dp[-1]
+            
         def check(left, right):
             if right - left == 2:
                 if nums[left] == nums[right-1]:
@@ -28,22 +40,19 @@ class Solution:
                 memo[key] = False
                 return memo[key]
 
-            if right - left == 2 or right - left == 3:
+            if right - left <= 3:
                 memo[key] = check(left, right)
                 return memo[key]
             
             if isValidate(left, left+2) and isValidate(left+2, right):
                 memo[key] = True
-                return memo[key]
 
             elif isValidate(left, left+3) and isValidate(left+3, right):
                 memo[key] = True
-                return memo[key]
+
             else:
                 memo[key] = False
-                return memo[key]
-
-        isValidate(0, n)
-        print(memo)
+            
+            return memo[key]
 
         return isValidate(0, n)
