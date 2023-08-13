61a72e913d417df26964d7626f29cf25963989f6
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-13 16:35:40 +0900

    Time: 275 ms (93.42%), Space: 18.8 MB (89.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,23 +4,21 @@ class Solution:
         n = len(grid[0])
         
         move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        visited = set()
         result = 0
 
         def dfs(x, y):
-            visited.add((x, y))
+            grid[x][y] = "0"
 
             for dx, dy in move:
                 nx, ny = x + dx, y + dy
                 
                 if 0 <= nx < m and 0 <= ny < n \
-                    and grid[nx][ny] == "1" \
-                    and not (nx, ny) in visited:
+                    and grid[nx][ny] == "1":
                     dfs(nx, ny)
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1" and not (i, j) in visited:
+                if grid[i][j] == "1":
                     dfs(i, j)
                     result += 1
         
