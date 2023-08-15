8e3204315a2ab8f2172d007d1fac171885ef5b8c
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-16 03:00:22 +0900

    Time: 48 ms (56.65%), Space: 16.2 MB (98.91%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,41 +5,22 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        if not head:
-            return head
-
         node = head
-        sm_head = None
-        sm_pointer = None
-        lg_head = None
-        lg_pointer = None
+        sm_head = sm_pointer = ListNode(0)
+        lg_head = lg_pointer = ListNode(0)
 
         while node:
-            if sm_head and node.val < x:
+            if node.val < x:
                 sm_pointer.next = node
                 sm_pointer = sm_pointer.next
-            elif lg_head and node.val >= x:
+            elif node.val >= x:
                 lg_pointer.next = node
                 lg_pointer = lg_pointer.next
-
-            elif not sm_head and node.val < x:
-                sm_pointer = node
-                sm_head = ListNode()
-                sm_head.next = sm_pointer
-            elif not lg_head and node.val >= x:
-                lg_pointer = node
-                lg_head = ListNode()
-                lg_head.next = lg_pointer
             
             node = node.next
         
-        if lg_pointer:
-            lg_pointer.next = None
+        lg_pointer.next = None
         
-        if sm_pointer and lg_head:
-            sm_pointer.next = lg_head.next
+        sm_pointer.next = lg_head.next
 
-        if sm_head:
-            return sm_head.next
-        
-        return head
+        return sm_head.next
