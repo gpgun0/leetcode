28996c3fc593ecf165d5882477dc1404549bb142
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-21 23:30:40 +0900

    Time: 72 ms (47.29%), Space: 16.4 MB (75.82%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -3,17 +3,10 @@ class Solution:
         l = len(s)
 
         for i in range(1, l//2+1):
-            pre = s[:i]
+            temp = s[:i]
 
-            for j in range(i, l-i+1, i):
-                cur = s[j:j+i]
-
-                if cur == pre:
-                    pre = cur
-                else:
-                    break
-            else:
-                if j+i == l:
+            if l % i == 0:
+                if temp * (l//i) == s:
                     return True
 
         return False
