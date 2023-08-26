c050f5ccf34bde36233ade79cf3e20f637a1a197
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-08-27 02:14:54 +0900

    Time: 594 ms (71.67%), Space: 16.8 MB (63.27%) - LeetHub

--- 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.py
@@ -8,8 +8,10 @@ class Solution:
         INF = int(1e9)
         dp = [[INF]*n for _ in range(d+1)]
 
-        for i in range(n-1, -1, -1):
-            dp[d][i] = max(jobDifficulty[i:])
+        hardest = 0
+        for i in range(n-1, d-2, -1):
+            hardest = max(hardest, jobDifficulty[i])
+            dp[d][i] = hardest
 
         for day in range(d-1, 0, -1):
             for i in range(day-1, n-(d-day)):
@@ -18,6 +20,7 @@ class Solution:
                 for k in range(i, n-(d-day)):
                     hardest = max(hardest, jobDifficulty[k])
                     dp[day][i] = min(dp[day][i], hardest+dp[day+1][k+1])
-                    
-
+        
+        for x in dp:
+            print(x)
         return dp[1][0]
