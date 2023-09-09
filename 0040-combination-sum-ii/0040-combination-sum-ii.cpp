79486f4356b42003c4308f79f4c52c2ed803f7e8
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-10 04:48:23 +0900

    Time: 34 ms (8.72%), Space: 16.3 MB (10.65%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,29 +2,42 @@ class Solution {
 public:
     vector<vector<int>> answer;
     vector<int> path;
+    map<int, int> counter;
+    vector<vector<int>> counterList;
 
     void dfs(int remain, vector<int>& path, int start, vector<int>& candidates) {
         if (remain == 0) {
             answer.push_back(path);
             return;
         }
+        if (remain < 0) {
+            return;
+        }
 
-        for (int i=start; i<candidates.size(); i++) {
-            if (i > start && candidates[i-1] == candidates[i]) {
+        for (int i=start; i<counterList.size(); i++) {
+            vector<int> entry = counterList[i];
+
+            if (entry[1] <= 0) {
                 continue;
             }
-            if (remain-candidates[i] < 0) {
-                break;
-            }
 
-            path.push_back(candidates[i]);
-            dfs(remain-candidates[i], path, i+1, candidates);
+            counterList[i] = {entry[0], entry[1]-1};
+            path.push_back(entry[0]);
+            dfs(remain-entry[0], path, i, candidates);
             path.pop_back();
+            counterList[i] = {entry[0], entry[1]};
         }
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
+        for (int i=0; i<candidates.size(); i++) {
+            counter[candidates[i]]++;
+        }
+        
+        for (auto& entry : counter) {
+            counterList.push_back({entry.first, entry.second});
+        }
+
         dfs(target, path, 0, candidates);
 
         return answer;
