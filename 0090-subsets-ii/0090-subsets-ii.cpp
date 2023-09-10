f45f65f29e62c5e8916c7e39f3920f08f2ecbf0f
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-10 14:15:25 +0900

    Time: 4 ms (53.36%), Space: 8.2 MB (35.11%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> answer;
+    map<int, int> counter;
+    vector<vector<int>> counterList;
     vector<int> path;
 
     void dfs(vector<int> path, vector<int>& nums, int start) {
         answer.push_back(path);
         
-        for (int i=start; i<nums.size(); i++) {
-            if (i>start && nums[i] == nums[i-1]) continue;
+        for (int i=start; i<counterList.size(); i++) {
+           vector<int> entry = counterList[i];
+           int value = entry[0], count = entry[1];
 
-            path.push_back(nums[i]);
-            dfs(path, nums, i+1);
+           if (count <= 0) {
+               continue;
+           }
+
+            counterList[i] = {value, count-1};
+            path.push_back(value);
+            dfs(path, nums, i);
             path.pop_back();
+            counterList[i] = {value, count};
         }
+        return;
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        for (int num: nums) {
+            counter[num]++;
+        }
+
+        for (auto& [key, value]: counter) {
+            counterList.push_back({key, value});
+        }
+
         dfs(vector<int>(), nums, 0);
         return answer;
     }
