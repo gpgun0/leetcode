1e9b55b47d842fcb6d667ca353f45db9c27a64ee
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-10 13:50:24 +0900

    Time: 0 ms (100%), Space: 7.4 MB (50.14%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,18 +3,22 @@ public:
     vector<vector<int>> answer;
     vector<int> path;
 
-    void dfs(vector<int>& path, vector<int>& nums, int start) {
-        answer.push_back(path);
-        
-        for (int i=start; i<nums.size(); i++) {
-            path.push_back(nums[i]);
-            dfs(path, nums, i+1);
-            path.pop_back();
-        }
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        dfs(path, nums, 0);
+        answer.push_back(vector<int>());
+
+        for (int num: nums) {
+            vector<vector<int>> newSubset;
+            
+            for (vector<int> subset: answer) {
+                subset.push_back(num);
+                newSubset.push_back(subset);
+            }
+
+            for (vector<int> subset: newSubset) {
+                answer.push_back(subset);
+            }
+        }
+        
         return answer;
     }
 };
