79e4dd186a447391726a23311bff3bdb9d03552f
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-12 11:13:48 +0900

    Time: 794 ms (24.88%), Space: 202.4 MB (51.09%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    bool dfs(map<int, vector<int>>& graph, vector<bool>& seen, int curNode, int destination) {
-        if (curNode == destination) {
-            return true;
-        }
-        if (seen[curNode]) {
-            return false;
-        }
-
-        seen[curNode] = true;
-        for (auto nextNode: graph[curNode]) {
-            if (dfs(graph, seen, nextNode, destination)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         map<int, vector<int>> graph;
-        vector<bool> seen(n);
+        vector<bool> seen(n, 0);
+        queue<int> q;
 
         for (auto edge: edges) {
             int u = edge[0], v = edge[1];
             graph[u].push_back(v);
             graph[v].push_back(u);
         }
+        q.push(source);
+        seen[source] = 1;
 
-        return dfs(graph, seen, source, destination);
+        while (!q.empty()) {
+            int curNode = q.front();
+            q.pop();
+            if (curNode == destination) {
+                return true;
+            }
+
+            for (auto nextNode: graph[curNode]) {
+                if (seen[nextNode]) {
+                    continue;
+                }
+
+                seen[nextNode] = 1;
+                q.push(nextNode);
+            }
+        }
+
+        return false;
     }
 };
