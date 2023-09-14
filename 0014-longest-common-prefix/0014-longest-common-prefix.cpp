5d44df374e842bd895bf2c4041f8460ebb63a62e
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-14 10:34:45 +0900

    Time: 9 ms (11.09%), Space: 12.9 MB (7.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        for (int i=0; i<strs[0].length(); i++) {
-            char c = strs[0][i];
-            
-            for (int j=1; j<strs.size(); j++) {
-                if (i == strs[j].length() || strs[j][i] != c) {
-                    return strs[0].substr(0, i);
-                }
+
+    string commonPrefix(string left, string right) {
+        int m = min(left.length(), right.length());
+        for (int i=0; i<m; i++) {
+            if (left[i] != right[i]) {
+                return left.substr(0, i);
             }
         }
+        return left.substr(0, m);
+    }
+    string longestCommonPrefix(vector<string>& strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        }
 
-        return strs[0];
+        int mid = (l+r)/2;
+        string leftStr = longestCommonPrefix(strs, l, mid);
+        string rightStr = longestCommonPrefix(strs, mid+1, r);
+        
+        return commonPrefix(leftStr, rightStr);
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        return longestCommonPrefix(strs, 0, strs.size()-1);
     }
 };
