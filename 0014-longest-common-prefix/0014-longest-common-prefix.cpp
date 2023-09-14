4c02dd078110621ff0365c3901fad3012e2c31b6
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-14 10:15:49 +0900

    Time: 7 ms (18.49%), Space: 9.7 MB (11.37%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,14 +6,13 @@ public:
         for (int i=1; i<strs.size(); i++) {
             string cur = strs[i];
             int n = min(prev.length(), cur.length());
-
-            for (int j=0; j<n; j++) {
+            int j=0;
+            for (j=0; j<n; j++) {
                 if (prev[j] != cur[j]) {
-                    prev = prev.substr(0, j);
                     break;
                 }
             }
-            prev = prev.substr(0, n);
+            prev = prev.substr(0, j);
         }
 
         return prev;
