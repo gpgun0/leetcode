d2b82bb6afd017af1237738f2736519241d5c856
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-15 12:23:02 +0900

    Time: 3 ms (80.84%), Space: 6.7 MB (34.16%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -3,22 +3,12 @@ public:
     int count = 0;
 
     bool check(int row, int col, int n, vector<vector<int>>& board) {
-        for (int i=0; i<n; i++) {
-            if (i != col && board[row][i]) {
-                return false;
-            }
+        for (int i=0; i<row; i++) {
             if (i != row && board[i][col]) {
                 return false;
             }
         }
         int i=row, j=col;
-
-        while (i<n-1 && j<n-1) {
-            if (board[++i][++j]) {
-                return false;
-            }
-        }
-        i=row, j=col;
         while (i>0 && j>0) {
             if (board[--i][--j]) {
                 return false;
@@ -30,29 +20,21 @@ public:
                 return false;
             }
         }
-        i=row, j=col;
-        while (i<n-1 && j>0) {
-            if (board[++i][--j]) {
-                return false;
-            }
-        }
-
         return true;
     }
 
     void dfs(int row, int n, vector<vector<int>>& board) {
+        if (row == n) {
+            count += 1;
+            return;
+        }
 
         for (int col=0; col<n; col++) {
             if (check(row, col, n, board)) {
                 board[row][col] = 1;
-
-                if (row + 1 == n) {
-                    count += 1;
-                } else {
-                    dfs(row+1, n, board);
-                }
+                dfs(row+1, n, board);
+                board[row][col] = 0;
             }
-            board[row][col] = 0;
         }
 
         return;
@@ -62,7 +44,6 @@ public:
         vector<vector<int>> board(n, vector<int>(n, 0));
 
         dfs(0, n, board);
-
         return count;
     }
 };
