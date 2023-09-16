4a1dcc55e35be9801da875ff0a04f310c7429f93
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-17 03:06:42 +0900

    Time: 26 ms (18.18%), Space: 18.5 MB (11.39%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -2,27 +2,31 @@ class Solution {
 public:
     vector<vector<int>> answer;
     int destination;
-    vector<int> path = {0};
-
-    void dfs(vector<vector<int>>& graph, int curNode, vector<int>& path) {
-        if (curNode == destination) {
-            answer.push_back(path);
-            return;
-        }
-
-        for (auto nextNode :graph[curNode]) {
-            path.push_back(nextNode);
-            dfs(graph, nextNode, path);
-            path.pop_back();
-        }
-        return;
-    }
+    set<int> seen;
+    queue<vector<int>> q;
 
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         destination = graph.size() - 1;
+        
+        q.push({ 0 });
+        
+        while (!q.empty()) {
+            vector<int> path = q.front();
+            q.pop();
+            int curNode = path.back();
 
-        dfs(graph, 0, path);
-
+            for (auto nextNode: graph[curNode]) {
+                path.push_back(nextNode);
+                if (nextNode == destination) {
+                    answer.push_back(path);
+                }
+                if (!seen.count(nextNode)) {
+                    q.push(path);
+                }
+                
+                path.pop_back();
+            }
+        }
         return answer;
     }
 };
