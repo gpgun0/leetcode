decfd2c5a9a4be69e394a963c8b6a6834681057e
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-09-29 20:18:06 +0900

    Time: 147 ms (6.05%), Space: 96.7 MB (29.47%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true;
+        bool decreasing = true;
+
+        for (int i=0; i<nums.size()-1; i++) {
+            if (nums[i] > nums[i+1]) {
+                increasing = false;
+            }
+            if (nums[i] < nums[i+1]) {
+                decreasing = false;
+            }
+        }
+
+        return increasing | decreasing;
+    }
+};
