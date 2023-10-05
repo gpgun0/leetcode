a5434e4932d51ea3e0bd40b80b5a4469f0e2098f
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-10-05 23:34:16 +0900

    Time: 7 ms (97.64%), Space: 20 MB (7.98%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = NULL;
+        int count = 0;
+
+        for (int i=0; i<nums.size(); i++) {
+            if (count == 0) {
+                candidate = nums[i];
+            }
+
+            if (nums[i] == candidate) {
+                count += 1;
+            } else {
+                count -= 1;
+            }
+        }
+
+        return candidate;
+    }
+};
