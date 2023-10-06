d262cd956b504af3dd146e254ca74c2ea48df08e
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2023-10-06 23:45:49 +0900

    Time: 0 ms (100%), Space: 7.9 MB (9.22%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        map<int, int> count;
+        map<int, int> counts;
         int ans = 0;
         
-        for (auto num: nums) {
-            if (count[num]) {
-                count[num] += 1;
-            } else {
-                count[num] = 1;
-            }
+        for (int num: nums) {
+            ans += counts[num];
+            counts[num]++;
         }
-
-        for (auto it=count.begin(); it!=count.end(); it++) {
-            ans += (it->second)*(it->second-1)/2;
-        }
-
+        
         return ans;
     }
 };
