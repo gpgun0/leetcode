b0103164f52513be75ed680030355435fa8152ca
Kim-GeonHo <rjsgh7943@gmail.com>
2025-05-24 18:36:07 +0900

    Time: 0 ms (100%), Space: 17.9 MB (42.54%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        answer = []
+
+        for i in range(len(words)):
+            if x in words[i]:
+                answer.append(i)
+        
+        return answer
