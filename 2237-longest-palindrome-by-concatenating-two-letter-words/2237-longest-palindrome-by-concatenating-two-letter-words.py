0ba4b16b93bf66be11f54d68b6ae67d4b1bdeaea
Kim-GeonHo <rjsgh7943@gmail.com>
2025-05-25 16:59:01 +0900

    Time: 142 ms (34.84%), Space: 35.5 MB (92.42%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,28 +1,19 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        hash_map = defaultdict()
-        only_once = 0
+        hash_map = defaultdict(int)
         answer = 0
 
         for word in words:
             reversed_word = word[::-1]
 
-            if hash_map.get(reversed_word):
-                if hash_map.get(reversed_word) == 1:
-                    hash_map.pop(reversed_word)
-                else:
-                    hash_map[reversed_word] -= 1
+            if hash_map[reversed_word]:
+                hash_map[reversed_word] -= 1
                 answer += 4
             
             else:
-                if hash_map.get(word):
-                    hash_map[word] += 1
-                else:
-                    hash_map[word] = 1
+                hash_map[word] += 1
         
-        for key in hash_map:
-            if only_once == 0 and key[0] == key[1]:
-                answer += 2
-                only_once += 1
+        if any(k[0] == k[1] for k, v in hash_map.items() if v > 0):
+            answer += 2
 
         return answer
