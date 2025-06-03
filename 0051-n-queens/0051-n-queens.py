249233b51f4e58d5ae2ce33e62acf6c86f0da27b
Kim-GeonHo <rjsgh7943@gmail.com>
2025-06-03 16:15:06 +0900

    Time: 44 ms (15.45%), Space: 18.7 MB (14.87%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,63 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        output = []
+        board = ["."*n] * n
+        verticals = [False] * n 
+
+        def checkDiagonal(board, x, y):
+            i, j = x+1, y+1
+            while i < n and j < n:
+                print(i, j)
+                if board[i][j] == "Q":
+                    return False
+                i += 1
+                j += 1
+
+            i, j = x-1, y+1
+            while i >= 0 and j < n:
+                if board[i][j] == "Q":
+                    return False
+                i -= 1
+                j += 1    
+
+            i, j = x-1, y-1
+            while i >= 0 and j >= 0:
+                if board[i][j] == "Q":
+                    return False
+                i -= 1
+                j -= 1
+
+            i, j = x+1, y-1
+            while i < n and j >= 0:
+                if board[i][j] == "Q":
+                    return False
+                i += 1
+                j -= 1
+
+            return True            
+
+
+        def dfs(board, x, y):
+            if x == n:
+                output.append(board[:])
+                return
+
+            for y in range(n):
+                if verticals[y]:
+                    continue
+                if not checkDiagonal(board, x, y):
+                    continue
+                
+                temp = ["."]*n
+                temp[y] = "Q"
+                board[x] = "".join(temp)
+                verticals[y] = True
+
+                dfs(board, x+1, 0)
+
+                verticals[y] = False
+                board[x] = "." * n
+
+        dfs(board, 0, 0)
+
+        return output
