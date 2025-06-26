214c3748843e73ded5171636fe81a25ee2c77757
Kim-GeonHo <rjsgh7943@gmail.com>
2025-06-26 21:09:25 +0900

    Time: 1187 ms (14.73%), Space: 19.7 MB (29.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        INF = int(1e9)
-        m = len(coins)
-        coins = [0]+coins
+        coins = [0] + coins
+        n = len(coins)
+        dp = [[1e9]*(amount+1) for _ in range(n)]
 
-        dp = [0] + [INF]*amount
-
-        for i in range(1, m+1):
-            coin = coins[i]
-            for j in range(coin, amount+1):
-                dp[j] = min(dp[j], dp[j-coin]+1)
-
-        if dp[-1] == INF:
-            return -1
-        return dp[-1]
-
+        for i in range(1, n):
+            dp[i][0] = 0
         
+        for i in range(1, n):
+            coin = coins[i]
+            for j in range(1, amount+1):
+                if j < coin:
+                    dp[i][j] = dp[i-1][j]
+                    continue
+
+                dp[i][j] = min(dp[i-1][j-coin] + 1, dp[i][j-coin] + 1, dp[i-1][j])
+        
+        return dp[-1][-1] if dp[-1][-1] < 1e9 else -1
